8273b26f350b30d203285e7320cf942b4aa813fe
stevabra <steven4abraha@gmail.com>
2020-08-09 15:46:03 +0530

    cleared again

--- lambda_function.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello there!')
-    }
