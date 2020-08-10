ebb7c4e1784a12ac0281ffc35e8cdccc5dbcf104
stevabra <steven4abraha@gmail.com>
2020-08-10 18:06:59 +0530

    Added 3 min wait

--- index.js
+++ index.js
@@ -2,24 +2,23 @@ const Webex = require(`webex`);
 var moment = require('moment-timezone');
 
 
-//required ENV variables 
+//STEP 1 : required ENV variables in AWS
 //botId : botID from webex teams documentation
 //responseText : text to reply after hours 
 //roomId : space id 
 //timezone : timezone from moment.js site 
 
+//STEP 2 : change bot name in code 
+
+//STEP 3: ADD HOLIDAY LIST in code 
 
 
 //needs to customized for each country 
 var holidays=["August 11th 2020", "August 28th 2020","October 2nd 2020","October 26th 2020","November 16th 2020","December 25th 2020"]
 
 var timezone=process.env.timezone
-
-var previousAlertTiming=moment().tz(timezone)
-
-
-
 //var timezone="Asia/Kolkata"
+var previousAlertTiming=moment().tz(timezone)-190000
 
 function isWeekend(){
     var now = moment().tz(timezone)
@@ -35,9 +34,7 @@ function isPublicHoliday(){
 }
 
 function isOfficeHours(){
-
     var now = moment().tz(timezone)
-    
     if (now > moment.tz(moment().format('MMMM Do YYYY')+", 09:00:00 am", "MMMM Do YYYY, h:mm:ss a", timezone)){
         if (now < moment.tz(moment().format('MMMM Do YYYY')+", 05:00:00 pm", "MMMM Do YYYY, h:mm:ss a", timezone)){
             return true
@@ -64,8 +61,8 @@ exports.handler = function(event, context, callback){
         if ((isPublicHoliday()||isWeekend()||!isOfficeHours())&&(moment().tz(timezone)-previousAlertTiming>180000)){
             previousAlertTiming=moment().tz(timezone)
             webex.messages.create({
-                //markdown: ''+process.env.responseText+'',
-                markdown: event.data,
+                markdown: ''+process.env.responseText+'',
+                //markdown: "this is a test",
                 roomId: process.env.roomId
                 //roomId: "Y2lzY29zcGFyazovL3VzL1JPT00vN2NjMWU0ZDAtZGExZC0xMWVhLThiMDUtMTFjZDJhNmY3NTYy"
             })
@@ -77,7 +74,5 @@ exports.handler = function(event, context, callback){
     };
 
 
-callback(null,204)
+callback(null,200)
 }
-
-
